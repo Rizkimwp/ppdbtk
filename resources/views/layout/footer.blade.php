30cab4df7bf1957bb92be6e62d423cc873fa5275
Rizki Maulana <rizki.mwp404@gmail.com>
2024-07-26 22:39:46 +0700

    fix

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024 All rights
-            reserved.</span>
+            reserve.</span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">PENERIMAAN PESERTA DIDIK BARU<i
                 class="mdi mdi-heart text-danger"></i></span>
     </div>
