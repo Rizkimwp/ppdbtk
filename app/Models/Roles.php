4a0e44eab92c27c4249233d6b084b08079f865dd
Rizki Maulana <rizki.mwp404@gmail.com>
2024-07-26 23:07:03 +0700

    fix

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Role extends Model
+class Roles extends Model
 {
     use HasFactory;
     protected $fillable = [
